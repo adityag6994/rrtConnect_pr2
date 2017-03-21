0e6e63b469f25ce69e3265f33b62406d786c4e18
Aditya Gupta <agupta2@wpi.edu>
2017-03-21 13:29:52 -0400

    removed the testing part

--- myplugin.cpp
+++ myplugin.cpp
@@ -30,7 +30,6 @@ public:
         _penv = penv;
         RegisterCommand("MyCommand",boost::bind(&RRTModule::MyCommand,this,_1,_2),
                         "This is an example command hello");
-        //RegisterCommand("load",boost::bind(&RRTModule::Load, this,_1,_2),"loads a given file");
     }
     virtual ~RRTModule() {}
     
@@ -50,13 +49,8 @@ public:
         Lower Limits:  Config:  -0.564602 -0.3536 -2.12131 -2.00001 -10000 -2.00001 -10000 
         Upper Limits:  Config:   2.13539   1.2963 -0.15    -0.1      10000 -0.1      10000 
         */
-        // printConfig("Lower Limits: ",_lowerLimit);
-        // printConfig("Upper Limits: ",_upperLimit);
-        // cout << "start config .. :: " ;
-        //printConfig(_startConfig);
-        // printConfig(_goalConfig);
-        //we have goal and start config with us now
-        //add them to NodeTree
+        
+        //we have goal and start config with us now, add them to NodeTree
         _weight.push_back(6);
         _weight.push_back(5);
         _weight.push_back(4);
@@ -78,48 +72,31 @@ public:
 
         _randomConfig = _startConfig;
         //Main Algorithm Starts Here
-        //get a random config
         count = 0;
         while(count++ < 10){
             //------------------------get random node
-            // cout << "...................................... COUNT ::::: " <<count << endl;
             _randomConfig = randomnodeGenerator();
             printConfig("Random",_randomConfig);  
             
             
             //------------------------check for nearest node in tree
             _nearestNode = NNNode();
-            //cout << _mainTree.getTreeSize() << endl;
-            // printConfig("nearest config",_nearestNode->getConfig());
+            printConfig("Nearest", _randomConfig);
+
             cout << "{ count :: " << count << " || unique id :: " << _nearestNode->getUniqueId() << " }" << endl;
             
             
-            //------------------------Extend into that direction
+            //------------------------Get the step size
             _stepSize = step();
             //printConfig("Step",_stepSize);
-
+            
+            //------------------------get the next node
             EXTEND(count);
-            // printConfig("Extend",_stepSize);
-            // _mainTree.nodeAdd(Node(new RRTNode(_extendConfig_new, _nearestNode)));
-
-            //_extendNode_new = Node(new RRTNode());
-            //_extendNode_new->setConfig(_extendConfig_new);
-            //_extendNode_new->setParent(_nearestNode);
-
-            // cout << "Tree Size :: "<< _mainTree.getTreeSize() << endl;
-             // _mainTree.nodeAdd(newNode);
-
+            
             //------------------------
             cout << endl;
 
         }
-
-        vector< boost::shared_ptr<RRTNode> > temp_tree_1 = _mainTree.getfullPath();
-        
-        // for(size_t i=0; i<_mainTree.getTreeSize(); i++){
-        //     printConfig("stored config: ",temp_tree_1[i]->getConfig());
-        // }
-
 
         cout << "*PLUGIN FINISHED" << endl;   
 
@@ -127,6 +104,7 @@ public:
 
     }
 
+    //Print config function
     void printConfig(string s ,Config config){
         cout << s <<" Config:  " ;
         for(size_t i = 0; i < 7 ; i++){
@@ -134,6 +112,7 @@ public:
             }
             cout << endl;
     }
+
     //getStringVector
     vector<dReal> getStringVector(std::ostream& sout, std::istream& sinput){
         vector<std::string> words;
@@ -150,6 +129,7 @@ public:
         return goal_config;
     }
 
+    //check if config are in limits
     bool checkConfigLimits(Config config){
     /*
         Lower Limits:  Config:  -0.564602 -0.3536 -2.12131 -2.00001 -10000 -2.00001 -10000 
@@ -172,31 +152,24 @@ public:
     //generate random node
     Config randomnodeGenerator(){
 
-        //cout << "--Random Generator--" << endl;
         dReal bias= RaveRandomFloat();
         Config temp_config = _goalConfig;
-        // cout << "bias : " << bias << endl;
+        
         if(bias < GOAL_BIAS){
             temp_config = _goalConfig;
-            // printConfig("goal config: ",temp_config);
         }else{
             do{
-                // cout << "bias_ before ::: " << bias << endl;
-                // cout << "bias : " << bias << endl;
-                // printConfig("r: ",temp_config);
+
                 for (size_t i = 0; i < 7; ++i) {
-                    // printConfig("ra: ",temp_config);
-                    // cout << "bias ::: " << bias << endl;
+
                     temp_config[i] = static_cast<dReal>(bias*(_upperLimit[i] - _lowerLimit[i]) + _lowerLimit[i]);
                     bias= RaveRandomFloat();//change random configration next time <common sense , not so common :'D  :| >
-                    // cout << "bias ::: " << bias << endl;
-                    //printConfig("random: ",temp_config);
+
                    }    
                 }while(CheckCollision(temp_config));   
         }
-        // cout << "bias ::: " << bias << endl;
-        // printConfig("random config: ",temp_config);
         return temp_config; 
+    
     }
 
     //nearest node 
@@ -206,43 +179,26 @@ public:
         dReal min_val = 100;
         size_t range = _mainTree.getTreeSize();
         vector< boost::shared_ptr<RRTNode> > temp_tree = _mainTree.getfullPath();
-        // cout << "range :: " << range << endl;
+    
         for(size_t i=0; i<range; i++){
-            //Node temp_node = _mainTree[i];
-            
+    
             Config temp_config = temp_tree[i]->getConfig();
-
 
             for(size_t j=0; j<7; j++){
                 if(j == 4 || j == 6)
                   temp_val += pow((_randomConfig[j]/10000 - temp_config[j]/10000)*_weight[j], 2);
                 else
                   temp_val += pow((-temp_config[j] + _randomConfig[j])*_weight[j], 2);
-                // temp_val += (- temp_config[j] + _randomConfig[j])*_weight[j];// TODO j+1 for weight...can be opposite
-              // cout << "temp_val--- " << temp_val << endl;
             }
 
             temp_val = sqrt(temp_val);
             
-            // cout << "temp_val " << temp_val << endl;
-            // cout << "outside " << i << endl;
-             // cout << "temp_val " << temp_val << endl;
-            // cout << "min_index.....|||." << _nearestNode->getCurrentID() << endl;
-            // printConfig("nn ",temp_tree[i]->getConfig());
             if(temp_val < min_val){
-                // cout << "inseide " << i << endl;
-                // cout << "min_index................." << temp_tree[i]->getCurrentID() << endl;
-                // cout << "min_val " << min_val << endl;
+        
                 min_val = temp_val;
                 _nearestNode = temp_tree[i];
                 _nearestNode->setUniqueId(i);
-                // printConfig("nn-- ",_nearestNode->getConfig());
-                // printConfig("nn ",temp_tree[i]->getConfig());
-                //_nearestNode->getCurrentID() = temp_tree[i]->getCurrentID
-                // cout << "min_val " << min_val << endl;
-                // cout << "min_index................." << _nearestNode->getCurrentID() << endl;
-                // cout << "min_index................." << temp_tree[i]->getCurrentID() << endl;
-                // cout << "min_index................." << _nearestNode->getUniqueId() << endl;
+        
             }
         }
         printConfig("Nearest Config ::",_nearestNode->getConfig());
@@ -283,25 +239,19 @@ public:
             // printConfig("Before Adding Nearest Node", temp_new_config);
             temp_new_config = _nearestNode->getConfig();
             cout << "count " << count << endl;
-            printConfig("Before Adding", temp_new_config);
+            // printConfig("Before Adding", temp_new_config);
             // printConfig("Extend Config :", temp_new_config);
             
             if(_nearestNode->getConfig() == _goalConfig){
                 cout << "--------------------------------------------Found Goal" << endl;
             }
 
-        // do{
             for(size_t i=0; i<7 ; i++){
                 temp_new_config[i] += _stepSize[i];
                 //_robot->SetDOFValues(temp_new_config);
             }
-            printConfig("After Adding", temp_new_config);
+            // printConfig("After Adding", temp_new_config);
             
-            // printConfig("Step Config :", _stepSize);
-            // printConfig("New Config :", temp_new_config);
-
-            //Node _temp = Node(new RRTNode(temp_new_config, _nearestNode));
-
              _mainTree.nodeAdd(Node(new RRTNode(temp_new_config, _nearestNode)));
 
             if(!CheckCollision(temp_new_config)){
@@ -309,25 +259,10 @@ public:
                 _nearestNode = _mainTree.getLast();       
 
             }
-        // }while();
+
         }while(CheckCollision(temp_new_config));// && checkConfigLimits(temp_new_config));
 
-        //      _mainTree.nodeAdd(Node(new RRTNode(_extendConfig_new, _nearestNode)));
-        // }while(CheckCollision(temp_new_config)); 
-        // //printConfig("Extended", temp_new_config);
-        //return temp_new_config;
     }
-
-    //check condition for extend
-    // string Connect(){
-    //     string s;
-    //     do{
-    //            s = EXTEND(); 
-    //     }while(s == "GO");
-
-    //     return s;
-    // }
-
 
     bool CheckCollision(Config config){
         EnvironmentMutex& lock = _penv->GetMutex();
