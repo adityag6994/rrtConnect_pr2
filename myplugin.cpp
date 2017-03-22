e45c867fb980e4d9e210b267760de307abd8ffe6
Aditya Gupta <agupta2@wpi.edu>
2017-03-22 02:34:49 -0400

    working..plotting TODO

--- myplugin.cpp
+++ myplugin.cpp
@@ -18,7 +18,7 @@ using namespace std;
 size_t MAX_ITERATIONS = 100;
 int RRTNode::CurrentID = 0;
 dReal GOAL_BIAS = 0.2;
-dReal delta_Q = 0.05;
+dReal delta_Q = 0.4;
 int flag = 0;
 
 typedef std::vector<OpenRAVE::dReal> Config;
@@ -27,6 +27,12 @@ typedef boost::shared_ptr<NodeTree> Tree;
 
 class RRTModule : public ModuleBase
 {
+
+//startconfig = [-0.15,0.075,-1.008,0,0,-0.11,0]
+//goalconfig = [0.449,-0.201,-0.151,0,0,-0.11,0]
+
+
+
 public:
     RRTModule(EnvironmentBasePtr penv, std::istream& ss) : ModuleBase(penv) {
         _penv = penv;
@@ -43,18 +49,35 @@ public:
         _penv->GetRobots(_robots);
         _robot = _robots.at(0);
         _robot->GetActiveDOFValues(_startConfig);
-        _goalConfig = getStringVector(sout, sinput);
-         
+        // _goalConfig = getStringVector(sout, sinput);
+         _goalConfig.push_back(0.449);
+         _goalConfig.push_back(-0.201);
+         _goalConfig.push_back(-0.151);
+         _goalConfig.push_back(-0.11);
+         _goalConfig.push_back(0);
+         _goalConfig.push_back(-0.11);
+         _goalConfig.push_back(0);
+
+         printConfig("goal", _goalConfig);
          //get limits
         _robot->GetActiveDOFLimits(_lowerLimit, _upperLimit);
+
+        _lowerLimit[4] = -3.14;
+        _lowerLimit[6] = -3.14;
+        
+        _upperLimit[4] = 3.14;
+        _upperLimit[6] = 3.14;
+        
         /*
         Lower Limits:  Config:  -0.564602 -0.3536 -2.12131 -2.00001 -10000 -2.00001 -10000 
         Upper Limits:  Config:   2.13539   1.2963 -0.15    -0.1      10000 -0.1      10000 
         */
         
         //we have goal and start config with us now, add them to NodeTree
+        //3.17104, 2.75674, 2.2325, 1.78948, 0, 0.809013, 0
         _weight.push_back(3.17104);
         _weight.push_back(2.75674);
+        _weight.push_back(2.2325);
         _weight.push_back(2.2325);
         _weight.push_back(1.78948);
         _weight.push_back(0);
@@ -63,9 +86,10 @@ public:
 
 
 
+
         _startNode = Node(new RRTNode());
         _startNode->setConfig(_startConfig);
-        _startNode->setParent(NULL);
+        _startNode->setParent(nullptr);
         Config test = _startNode->getConfig();
         
         //adding start to tree
@@ -100,7 +124,16 @@ public:
                 
                 cout << "-------------------------------------------------------" <<endl;
                 // cout << "_diffGoal ---> "<< _diffGoal << endl;
-                _mainTree.printFullTree();
+                
+                // for(size_t i=0; i < _path.size(); i++){
+                //     Config temp = _path[i]->getConfig();
+                //     for(size_t j=0; j < 7 ; j++){
+                //         cout << temp[j] << ' ';
+                //     }
+                //     cout << endl;
+                // }
+
+
                 cout << "Goal Found" << endl;
                 break;
                 
@@ -121,11 +154,34 @@ public:
             cout << endl;
 
         }
-        cout << "SIZE =================> " << _mainTree.getTreeSize() << endl;
-        // ExecuteTrajectory();
-        cout << "*PLUGIN FINISHED" << endl;   
 
+        getPath();
+        cout << "SIZE =================> " << _mainTree.getTreeSize() << endl;
+        ExecuteTrajectory();
+        cout << "*PLUGIN FINISHED" << endl;   
+        printConfig("goal", _goalConfig);
+        printConfig("start", _startConfig);
+        // printConfig("gaoal", _ggoalConfig);
         return true;
+
+    }
+
+    //getPath from last found goal as nearest neibhour
+    void getPath(){
+        //save in _path
+
+        // _goalNode = Node(new RRTNode());
+        // _goalNode->setConfig(_goalConfig);
+        // _goalNode->setParent(_nearestNode);
+
+        //_goalConfig->setParent(_nearestNode);
+        _path.push_back(_nearestNode);
+
+        while(_nearestNode->getParent() != nullptr){
+            _nearestNode = _nearestNode->getParent();
+            _path.push_back(_nearestNode);
+        }
+
 
     }
 
@@ -190,10 +246,10 @@ public:
                 for (size_t i = 0; i < 7; ++i) {
 
                     temp_config[i] = static_cast<dReal>(bias*(_upperLimit[i] - _lowerLimit[i]) + _lowerLimit[i]);
-                    bias= RaveRandomFloat();//change random configration next time <common sense , not so common :'D  :| >
+                    bias = RaveRandomFloat();//change random configration next time <common sense , not so common :'D  :| >
 
                    }    
-                }while(!CheckCollision(temp_config));   
+                }while(CheckCollision(temp_config)||!inLimits(temp_config));   
         }
         return temp_config; 
     
@@ -203,7 +259,7 @@ public:
    boost::shared_ptr<RRTNode> NNNode(){
     
         dReal temp_val = 0;
-        dReal min_val = 100;
+        dReal min_val = 1000000;
         dReal diff;
         size_t range = _mainTree.getTreeSize();
         vector< boost::shared_ptr<RRTNode> > temp_tree = _mainTree.getfullPath();
@@ -213,9 +269,9 @@ public:
             Config temp_config = temp_tree[i]->getConfig();
 
             for(size_t j=0; j<7; j++){
-                if(j == 4 || j == 6)
-                  temp_val += pow((_randomConfig[j]/10000 - temp_config[j]/10000)*_weight[j], 2);
-                else
+                // if(j == 4 || j == 6)
+                //   temp_val += pow((_randomConfig[j] - temp_config[j])*_weight[j], 2);
+                // else
                   temp_val += pow((-temp_config[j] + _randomConfig[j])*_weight[j], 2);
             }
 
@@ -248,15 +304,31 @@ public:
         dReal temp_val;
         
         for(size_t j=0; j<7; j++){
-                if(j == 4 || j == 6)
-                  temp_val += pow((config[j]/10000 - _goalConfig[j]/10000)*_weight[j], 2);
-                else
-                  temp_val += pow((-_goalConfig[j] + config[j])*_weight[j], 2);
+                // if(j == 4 || j == 6)
+                //   temp_val += pow((config[j]/10000 - _goalConfig[j]/10000), 2);//*_weight[j]
+                // else
+                  temp_val += pow((-_goalConfig[j] + config[j]), 2);//*_weight[j]
             }
 
             temp_val = sqrt(temp_val);
             return temp_val;
    }
+    
+
+    dReal differencebetweenTwo(Config config){
+        dReal temp_val;
+        for(size_t j=0; j<7; j++){
+                // if(j == 4 || j == 6)
+                //   temp_val += pow((config[j]/10000 - _randomConfig[j]/10000), 2);//*_weight[j]
+                // else
+                  temp_val += pow((-_randomConfig[j] + config[j]), 2);//*_weight[j]
+            }
+
+            temp_val = sqrt(temp_val);
+            return temp_val;    
+    }
+   
+
 
    //function for printing
     void printThis(string s,dReal g){
@@ -272,7 +344,11 @@ public:
         Config temp_step = _randomConfig;
         dReal magnitude = 0.0;
 
-        _robot->SubtractActiveDOFValues(temp_step, _nearestNode->getConfig());
+        for(size_t i=0 ; i<7 ; i++){
+            temp_step[i] = _randomConfig[i] - _nearestNode->getConfig()[i];
+        }
+        // _robot->SubtractActiveDOFValues(temp_step, _nearestNode->getConfig()); //change
+        // //range  
 
         for(size_t i=0; i<7; i++){
             magnitude += pow(temp_step[i], 2.0);
@@ -280,7 +356,7 @@ public:
         magnitude = sqrt(magnitude);
 
         for (size_t i = 0; i < 7; ++i) {
-            temp_step[i] = (temp_step[i] / magnitude) * delta_Q;
+            temp_step[i] = ((temp_step[i]) * delta_Q) / magnitude;
         }
 
         return temp_step;
@@ -301,82 +377,78 @@ public:
         // do{
             // printConfig("Before Adding Nearest Node", temp_new_config);
             temp_new_config = _nearestNode->getConfig();
-            // cout << "count " << count << endl;
-            // printConfig("Before Adding", temp_new_config);
-            // printConfig("Extend Config :", temp_new_config);
             
-            // if(_nearestNode->getConfig() == _goalConfig){
-            //     cout << "--------------------------------------------Found Goal" << endl;
-            //     flag = 1;
-            // }
-
-            // if(flag == 0){
-            //     cout << " searching ... " << count << endl; 
-            // }else{
-            //     break;
-            //     cout << " GOAL Found " ;
-            // }
-
             for(size_t i=0; i<7 ; i++){
                 temp_new_config[i] += _stepSize[i];
                 //_robot->SetDOFValues(temp_new_config);
             }
             // printConfig("After Adding", temp_new_config);
             
+            if(!inLimits(temp_new_config) || CheckCollision(temp_new_config)){
+                cout << "Out of Limits" << endl;
+                break;
+            }
+
              _mainTree.nodeAdd(Node(new RRTNode(temp_new_config, _nearestNode)));
 
-            // if(temp_new_config == _goalConfig){
-            //     return false;
-            // }else{
-            //     return true;
+            // if(!CheckCollision(temp_new_config) && inLimits(temp_new_config)){
+
+                _nearestNode = _mainTree.getLast();       
+
             // }
-            _diffGoal = differenceCost(temp_new_config);
+            
+
+            _diffConfig = differencebetweenTwo(temp_new_config);
+
+
+
+            if(_diffConfig < delta_Q){
+                _mainTree.nodeAdd(Node(new RRTNode(_randomConfig, _nearestNode)));
+                // printConfig("final config",)
+                //cout << _diffGoal << endl;
+                //flag = 1;
+                //break;
+                _diffGoal = differenceCost(_randomConfig);
+
             if(_diffGoal < delta_Q){
+                _mainTree.nodeAdd(Node(new RRTNode(_goalConfig, _nearestNode)));
                 // printConfig("final config",)
                 cout << _diffGoal << endl;
                 flag = 1;
                 break;
-            }
-
-            if(!CheckCollision(temp_new_config)){
-                 //_mainTree.nodeAdd(_temp);
-                // cout << " added ... " << endl;
-                _nearestNode = _mainTree.getLast();       
 
             }
 
-
-
-        }while(!CheckCollision(temp_new_config));// && checkConfigLimits(temp_new_config));
+            }
+ 
+        }while(!CheckCollision(temp_new_config) && inLimits(temp_new_config));// && checkConfigLimits(temp_new_config));
         // return s;
         // return true;
     }
 
 
+    bool inLimits(Config config){
+        for(size_t i=0; i<7; i++){
+            if(config[i] < _lowerLimit[i] || config[i] > _upperLimit[i]){
+                return false;
+            }
+        }
+        return true;
 
-    // bool extendExtend(int count){
-    //     string pp;
-    //     int j = count;
-    //     pp = Extend(count);
-
-    //     if(pp == "Searching .."){
-    //         return true;
-    //     }else{
-    //         return false;
-    //     }
-    // }
+    }
 
     bool CheckCollision(Config config){
-        EnvironmentMutex& lock = _penv->GetMutex();
-        lock.lock();
-        // if(config[5] < -2.00001){
-        //     config[5] = -2.00000;
+        // EnvironmentMutex& lock = _penv->GetMutex();
+        // lock.lock();
+        // // if(config[5] < -2.00001){
+        // //     config[5] = -2.00000;
         // }
         _robot->SetActiveDOFValues(config);
         bool check = _penv->CheckCollision(_robot);
         _robot->SetActiveDOFValues(_startConfig);
-        lock.unlock();
-        //cout << "----" << endl;
+        // cout << check << endl;
+        // lock.unlock();
+        // cout << "----" << endl;
         return check;
     }
 
@@ -387,12 +459,19 @@ public:
         TrajectoryBasePtr traj = RaveCreateTrajectory(_penv);
         traj->Init(_robot->GetActiveConfigurationSpecification());
 
-        vector< boost::shared_ptr<RRTNode> > temp_tree = _mainTree.getfullPath();
+        //vector< boost::shared_ptr<RRTNode> > temp_tree = _path;
 
-        for(int i=0; i< _mainTree.getTreeSize(); i++){
-            traj->Insert(0, temp_tree[i]->getConfig());
+        for(size_t i=0; i< _path.size(); i++){
+            // traj->Insert(0, _ggoalConfig);
+
+            for(size_t j=0; j<7 ; j++){
+                cout << _path[i]->getConfig()[j] << ' ';
+            }
+            cout << endl;
+            traj->Insert(0, _path[i]->getConfig());
         } 
             traj->Insert(0, _startConfig);
+            // traj->Insert(0, _ggoalConfig);
             planningutils::RetimeActiveDOFTrajectory(traj, _robot);
             _robot->GetController()->SetPath(traj);
             lock.unlock();
@@ -415,17 +494,22 @@ private:
     Config _stepSize; //get from config
     Config _extendConfig_new; //has new config from extend function
     Config _weight; //weight matrix used while calculating cost
+    Config _ggoalConfig;    
 
     Node _startNode;
     Node _nearestNode;
     Node _extendNode_new;
+    Node _goalNode;
     // Node _temp;
 
     NodeTree _mainTree;
     
     dReal _diffGoal;
-
+    dReal _diffConfig;
+    
     size_t count;
+
+    vector< boost::shared_ptr<RRTNode> > _path; 
 };
 
 
